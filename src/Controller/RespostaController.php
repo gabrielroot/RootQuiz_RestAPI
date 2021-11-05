5f30d8655d866c01861ef89d35f826ef44d67f7c
gabrielroot <gabrielfer.s88@gmail.com>
2021-11-05 18:12:35 -0300

    Refact: Removendo imports descecessários && Regra de negócio implementada

--- src/Controller/RespostaController.php
+++ src/Controller/RespostaController.php
@@ -2,12 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Pergunta;
 use App\Entity\Resposta;
-use App\Entity\Tentativa;
-use App\Entity\Usuario;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Pergunta;
 use App\Entity\Usuario;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
